692089a6fc05d4bf94449f4b1ed1e90cb81fece1
Artezon <artezon2003@gmail.com>
2026-06-16 10:37:07 +0300

    Enhance icons and add new ones

---
+++ src/components/icons/DownIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="4 4 16 16">
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2.4"
+      d="m8 10 4 4 4-4"
+    />
+  </svg>
+</template>
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,7 +3,14 @@ export { default as GenerateIcon } from "./GenerateIcon.vue";
 export { default as ImageIcon } from "./ImageIcon.vue";
 export { default as SearchIcon } from "./SearchIcon.vue";
 export { default as StopIcon } from "./StopIcon.vue";
+export { default as AddFolderIcon } from "./AddFolderIcon.vue";
 export { default as DeleteIcon } from "./DeleteIcon.vue";
+export { default as RefreshIcon } from "./RefreshIcon.vue";
+export { default as PauseIcon } from "./PauseIcon.vue";
+export { default as PlayIcon } from "./PlayIcon.vue";
+export { default as CloseIcon } from "./RemoveFromLibraryIcon.vue";
+export { default as UpIcon } from "./UpIcon.vue";
+export { default as DownIcon } from "./DownIcon.vue";
 
 export { default as WindowMinimize } from "./titlebar/WindowMinimize.vue";
 export { default as WindowMaximize } from "./titlebar/WindowMaximize.vue";
